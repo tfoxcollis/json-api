283457c5cdaa33ceee1db096f2bcafd6ef31fb6e
Trish Fox-Collis <tfoxcollis@gmail.com>
2023-01-15 11:39:11 -0800

    update vercel.json

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var bodyParser = require("body-parser")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+app.options('*', cors())
 app.use(cors())
 var fs = require('fs')
 
